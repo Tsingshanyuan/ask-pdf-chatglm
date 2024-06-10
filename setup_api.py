8778010a0d4c4c29e24c65f584c6ddd765e8e02f
Zhang Ziwen <61439192+Tsingshanyuan@users.noreply.github.com>
2024-06-10 23:17:20 +0800

    成功封装了zhipuai_embeddings，并且长PDF也能用了

--- setup_api.py
+++ setup_api.py
@@ -21,15 +21,14 @@ from use_ernie import get_llm
 from use_ernie import get_embeddings
 
 # 使用chatglm配合qianfan
-# 速度很快，pdf长度有限制
+# 速度很快，pdf长度有限制，我有qianfan的额度
 from use_chatglm import get_llm
 from use_qianfan import get_embeddings
 '''
 
-# 使用openai的API
-# 速度很快，不用翻墙，pdf可以很大，但是很贵，消耗的token很多
-from use_openai import get_llm
-from use_openai import get_embeddings
+# 使用chatglm
+from use_chatglm import get_llm
+from use_chatglm import get_embeddings
 
 
 # ------------------------------------------------------------------------------
--- use_chatglm.py
+++ use_chatglm.py
@@ -1,5 +1,10 @@
 from langchain_community.chat_models import ChatZhipuAI
+from zhipuai_embedding import ZhipuAIEmbeddings
 
 def get_llm():
     llm = ChatZhipuAI()
     return llm
+
+def get_embeddings():
+    embeddings = ZhipuAIEmbeddings()
+    return embeddings
